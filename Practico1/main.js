70d362add54af17764719bb5ef9edc8769d62600
catafr <catafarias@gmail.com>
2019-09-04 23:19:53 -0300

    entregable1 terminado

--- Practico1/main.js
+++ Practico1/main.js
@@ -1,7 +1,7 @@
 const canvas = document.getElementById("canvas"); //para q sea responsive el canvas y mantenga proporcion
 let heightRatio = 1.5;
 
-canvas.width = 800;
+canvas.width = 1024;
 canvas.height = canvas.width * heightRatio;
 
 //************************************ EJERCICIO 1  *********************************************************
@@ -100,41 +100,46 @@ PromedioFila(matriz, prom);
 //************** EJERCICIO 2 *****************************************************************************************
 /// NOTE: Pintar una región rectangular de un color utilizando el Contexto de HTML5.
 
-let ctx = canvas.getContext("2d");
-ctx.font = "bold 22px sans-serif";
-ctx.fillText("Ejercicio 2 ",20,20);
-ctx.fillStyle = "#f00";
-ctx.fillRect(50,50,200,100);
+let ctx2 = canvas.getContext("2d");
+ctx2.font = "bold 22px sans-serif";
+ctx2.fillText("Ejercicio 2 ",20,20);
+ctx2.fillStyle = "#f00";
+ctx2.fillRect(50,50,200,100);
 
 
 //*********************** EJERCICIO 3 *************************************************************************************
 /// NOTE: Pintar una región rectangular de un color utilizando la estructura de ImageData.
+let ctx3 = canvas.getContext("2d");
+ctx3.font = "bold 22px sans-serif";
+ctx3.fillStyle = "#000";
+ctx3.fillText("Ejercicio 3 ",20,200);
 
-ctx.font = "bold 22px sans-serif";
-ctx.fillStyle = "#000";
-ctx.fillText("Ejercicio 3 ",20,200);
-
-let imageData = ctx.createImageData(200,100);
+function dibujarRect(){
+let imageData3 = ctx3.createImageData(200,100);
   for(x=0; x<200; x++){
     for(y=0;y<100; y++){
-      setPixel(imageData,x,y,0,190,0,255);
+      setPixel(imageData3,x,y,0,190,0,255);
     }
   }
-  ctx.putImageData(imageData,50,220);
-
-function setPixel (imageData, x, y, r, g, b, a){
-  index = (x+y * imageData.width) * 4;
-  imageData.data[index+0] = r;
-  imageData.data[index+1] = g;
-  imageData.data[index+2] = b;
-  imageData.data[index+3] = a;
+  ctx3.putImageData(imageData3,50,220);
 }
+
+function setPixel (imageData3, x, y, r, g, b, a){
+  index = (x+y * imageData3.width) * 4;
+  imageData3.data[index+0] = r;
+  imageData3.data[index+1] = g;
+  imageData3.data[index+2] = b;
+  imageData3.data[index+3] = a;
+}
+
+dibujarRect();
 
 //***************************EJERCICIO 4 ************************************************************************************
 /// NOTE: Especificar la función para pintar un rectángulo utilizando un gradiente vertical de negro a blanco
-ctx.font = "bold 22px sans-serif";
-ctx.fillStyle = "#000";
-ctx.fillText("Ejercicio 4 ",20,380);
+let ctx4 = canvas.getContext("2d");
+ctx4.font = "bold 22px sans-serif";
+ctx4.fillStyle = "#000";
+ctx4.fillText("Ejercicio 4 ",20,380);
 
 function dibujarGradHorizontal(){
 
@@ -142,7 +147,7 @@ let ancho = 400;
 let alto = 200;
 let color = 0;
 
-let imageData4 = ctx.createImageData(ancho,alto);
+let imageData4 = ctx4.createImageData(ancho,alto);
 
 for ( x = 0; x < ancho; x++) {
 for ( y = 0; y < alto; y++) {
@@ -157,83 +162,96 @@ for ( y = 0; y < alto; y++) {
 
 }
 
-
-ctx.putImageData(imageData4,50,400)
+ctx4.putImageData(imageData4,50,400)
 }
+
 dibujarGradHorizontal();
 
-//******************************* EJERCICIO 5 *****************************************************************************************
-/// NOTE: Pintar un rectángulo en pantalla, utilizando un gradiente que vaya de negro a amarillo en la primera mitad del ancho del rectángulo,
+// //******************************* EJERCICIO 5 *****************************************************************************************
+// NOTE: Pintar un rectángulo en pantalla, utilizando un gradiente que vaya de negro a amarillo en la primera mitad del ancho del rectángulo,
 // y de amarillo a rojo, en la segunda mitad. Por otro lado, en Y el degrade se mantiene constante.
-ctx.font = "bold 22px sans-serif";
-ctx.fillStyle = "#000";
-ctx.fillText("Ejercicio 5 ",20,650);
 
- function dibujarGradVertical(){
+let ctx5 = canvas.getContext("2d");
+ctx5.font = "bold 22px sans-serif";
+ctx5.fillStyle = "#000";
+ctx5.fillText("Ejercicio 5 ",20,650);
 
-let ancho = 400;
-let alto = 200;
-let color = 0;
+// function dibujarGradVertical(){
+//   let ancho = 400;
+//   let alto = 200;
+//   let color = 0;
+//
+//   var imageData5 = ctx5.createImageData(ancho, alto, color);
+//
+//   for ( x=0; x<ancho*0.5; x++){
+//      color=x/(ancho*0.5)*255;
+//     for (y=0; y<alto; y++){
+//       setPixel(imageData5, x, y, color, color, 0);
+//     }
+//   }
+//
+//   for (x=ancho/2; x<ancho; x++){
+//     color = (1-(x-ancho*0.5)/(ancho*0.5))*255;
+//     for(y=0;y<alto;y++){
+//       setPixel(imageData5, x, y, 255, color, 0);
+//     }
+//   }
+//
+//   ctx5.putImageData(imageData5, 50, 900);
+// }
+//
+// function setPixel (imageData5, x, y, r, g, b ){
+//   index = (x+y * imageData5.width) * 4;
+//   imageData5.data[index+0] = r;
+//   imageData5.data[index+1] = g;
+//   imageData5.data[index+2] = b;
+//
+// }
+//
+// dibujarGradVertical();
+
+var gradient = ctx5.createLinearGradient(50,0, 250,0);
+
+// Add three color stops
+gradient.addColorStop(0, 'black');
+gradient.addColorStop(.5, 'yellow');
+gradient.addColorStop(1, 'red');
+
+// Set the fill style and draw a rectangle
+ctx5.fillStyle = gradient;
+ctx5.fillRect(50, 700, 200, 100);
 
 
-let imageData5 = ctx.createImageData(ancho,alto);
+//**********************EJERCICIO 6 *****************************************************************************************
+let ctx6 = canvas.getContext("2d");
+ctx6.font = "bold 22px sans-serif";
+ctx6.fillStyle = "#000";
+ctx6.fillText("Ejercicio 6 ",20,900);
 
-for ( x=0; x<ancho*0.5; x++){
-   color=x/(ancho*0.5) *255;
-  for ( y=0; y<alto; y++){
-    setPixel(imageData5, x, y, color, color, 0);
-  }
-}
+let greyScale = new Image();
+greyScale.src = "bacalar.jpg";
 
-for ( x=ancho/2; x<ancho; x++){
-  color = (1-(x-ancho*0.5)/(ancho*0.5))*255;
-  for(y=0;y<alto;y++){
-    setPixel(imageData5, x, y, 255, color, 0);
-  }
-}
-
-function setPixel(imageData5, x, y, r, g, b){
-index= (x+y*imageData5.width)*4;
-imageData.data[index+0]=r;
-imageData.data[index+1]=g;
-imageData.data[index+2]=b;
-
-}
-
-ctx.putImageData(imageData5, 50, 700);
-}
-
-dibujarGradVertical();
-
-//******************************************************* punto5 *****************************************************************************************
-//******************************************************* punto6 *****************************************************************************************
-
-let koalaimage = new Image();
-koalaimage.src = "koala.jpg";
-
-koalaimage.onload = function(){
+greyScale.onload = function(){
   myDrawImageMethod(this);
 }
 function myDrawImageMethod(img){
-  ctx.drawImage(img, 500, 400);
-  let w = img.width;
-  let h = img.height;
+  ctx6.drawImage(img, 50, 950);
+  let ancho = img.width;
+  let alto = img.height;
 
-  let imageData2 = ctx.getImageData(500,400, img.width, img.height);
-  let avg;
-  let index;
-  for (let x = 0; x < w; x++) {
-      for (let y = 0; y < h; y++) {
-          index = (x + y * w) * 4;
-          avg = (imageData2.data[index] + imageData2.data[index+1]  + imageData2.data[index+2]) / 3;
-          imageData2.data[index] = avg;
-          imageData2.data[index+1] = avg;
-          imageData2.data[index+2] = avg;
+  let imageData6 = ctx6.getImageData(50,950, img.width, img.height);
+  let gris;
+
+  for ( x = 0; x < ancho ; x++) {
+      for (y = 0; y < alto; y++) {
+        let index = (x + y * ancho) * 4;
+          gris = (imageData6.data[index] + imageData6.data[index+1]  + imageData6.data[index+2]) / 3;
+          imageData6.data[index] = gris;
+          imageData6.data[index+1] = gris;
+          imageData6.data[index+2] = gris;
 
       }
 
   }
-  ctx.putImageData(imageData2,500,400);
+  ctx6.putImageData(imageData6,50,950);
 }
-
-//******************************************************* punto6 *****************************************************************************************
